fe119b361a2d8b6ace8147552c719da039830f65
Jan Hlavac <jan@postsharp.net>
2022-05-06 11:37:35 +0200

    #30315 Updated to the newest Engineering.

--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -3,19 +3,30 @@
 
 using PostSharp.Engineering.BuildTools;
 using PostSharp.Engineering.BuildTools.Build.Model;
+using PostSharp.Engineering.BuildTools.Build.Solutions;
 using PostSharp.Engineering.BuildTools.Dependencies.Model;
 using Spectre.Console.Cli;
-using System.Collections.Immutable;
 
-var product = new Product
+var productDependencyDefinition = new DependencyDefinition( "Metalama.Open.DependencyEmbedder", VcsProvider.GitHub, "Metalama" )
 {
-    ProductName = "Metalama.Open.DependencyEmbedder",
-    Solutions = ImmutableArray.Create<Solution>( new DotNetSolution( "Metalama.Open.DependencyEmbedder.sln" ) ),
+    CiBuildTypes = new ConfigurationSpecific<string>(
+        "Metalama_MetalamaOpen_MetalamaOpenDependencyEmbedder_DebugBuild",
+        "Metalama_MetalamaOpen_MetalamaOpenDependencyEmbedder_ReleaseBuild",
+        "Metalama_MetalamaOpen_MetalamaOpenDependencyEmbedder_PublicBuild" ),
+    DeploymentBuildType = "Metalama_MetalamaOpen_MetalamaOpenDependencyEmbedder_PublicDeployment",
+    BumpBuildType = "Metalama_MetalamaOpen_MetalamaOpenDependencyEmbedder_VersionBump"
+};
+
+var product = new Product( productDependencyDefinition )
+{
+    Solutions = new Solution[] { new DotNetSolution( "Metalama.Open.DependencyEmbedder.sln" ) },
     PublicArtifacts = Pattern.Create( "Metalama.Open.DependencyEmbedder.$(PackageVersion).nupkg" ),
-    Dependencies = ImmutableArray.Create( Dependencies.PostSharpEngineering, Dependencies.Metalama )
+    Dependencies = new[] { Dependencies.PostSharpEngineering, Dependencies.Metalama },
+    RequiresBranchMerging = true
 };
 
 var commandApp = new CommandApp();
+
 commandApp.AddProductCommands( product );
 
 return commandApp.Run( args );
